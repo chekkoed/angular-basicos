eecffc8184835b09554a9d212879e8e359d77d47
chekkoed <chekko.ed@gmail.com>
2022-06-06 16:27:53 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//Tarea:
+//Crear un modulo llamado ContadorModule
+//declaraciones y exportaciones
 import { AppComponent } from './app.component';
+import { heoresModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heoresModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
